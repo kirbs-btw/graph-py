e51293056f6df2b891743ea16872d81a076bd950
Bastian Lipka <lipka.bastian04@gmail.com>
2025-10-07 23:54:23 +0200

    feat: algorithm structures

--- src/graph_py/core.py
+++ src/graph_py/core.py
@@ -20,12 +20,12 @@ class Graph(BaseModel):
     The question is going: 
     graph centric, node centric or edge centric? 
     Or somehow doing a pointer to the other ones to have all the representations
-    
 
     """
     name: Optional[str] = None
     id: str
     nodes: Optional[list[Node]] = []
+    edges: Optional[list[Edge]] = []
 
 __all__ = ["Graph", "Edge", "Node"]
 

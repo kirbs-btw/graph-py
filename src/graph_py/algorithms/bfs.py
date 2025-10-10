3049237c1d0d907a6174d6bde407ad9a6172813b
Bastian Lipka <lipka.bastian04@gmail.com>
2025-10-10 23:56:28 +0200

    fix: bfs issue search

--- src/graph_py/algorithms/bfs.py
+++ src/graph_py/algorithms/bfs.py
@@ -1,4 +1,4 @@
-from ..core import Graph, Node
+from ..core import Graph, Node, Edge
 
 
 def bfs(graph: Graph, src_node: Node, trg_node: Node):
@@ -10,11 +10,18 @@ def bfs(graph: Graph, src_node: Node, trg_node: Node):
     # checking that both nodes are in the same graph 
     assert (src_node in graph.nodes and trg_node in graph.nodes), "target and source node need to be in the same graph"
 
+    edges: list[Edge] = src_node.edges()
+
+    # checking if the trg_node is in the neighborhood
+    
+    # getting the one layer deep 
+    is_hood = False
+    for edge in edges:
+        if trg_node == edge.target:
+            is_hood = True
+    
+
     # getting the neighborhood of a node
     # checking if the neighborhood contains the trg_node
     # if not repeat for every node in the neighbor hood and do an expainsion throught the graph
-    # if yes backtrack the path
-    
-    # so what is missing 
-    # I would like to use a get neighborhood function for a node 
-    
+    # if yes backtrack the path 

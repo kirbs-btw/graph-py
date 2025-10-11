76e8765f15b8d45c9485becaf28715adf5f805cc
Bastian Lipka <lipka.bastian04@gmail.com>
2025-10-11 23:49:29 +0200

    feat: bfs

--- src/graph_py/algorithms/bfs.py
+++ src/graph_py/algorithms/bfs.py
@@ -1,27 +1,33 @@
-from ..core import Graph, Node, Edge
+from collections import deque
+from typing import Optional, List
+
+from ..core import Graph, Node
 
 
-def bfs(graph: Graph, src_node: Node, trg_node: Node):
-    """
-    Breadth-first search
-    """
+def bfs(graph: Graph, src_node: Node, trg_node: Node) -> Optional[List[Node]]:
+    """Run breadth-first search and return the shortest path as nodes."""
+    assert (
+        src_node in graph.nodes and trg_node in graph.nodes
+    ), "target and source node need to be in the same graph"
 
+    if src_node.id == trg_node.id:
+        return [src_node]
 
-    # checking that both nodes are in the same graph 
-    assert (src_node in graph.nodes and trg_node in graph.nodes), "target and source node need to be in the same graph"
+    adjacency = graph.adjacency
+    id_to_node = {node.id: node for node in graph.nodes}
 
-    edges: list[Edge] = src_node.edges()
+    visited = {src_node.id}
+    queue = deque([(src_node.id, [src_node.id])])
 
-    # checking if the trg_node is in the neighborhood
-    
-    # getting the one layer deep 
-    is_hood = False
-    for edge in edges:
-        if trg_node == edge.target:
-            is_hood = True
-    
+    while queue:
+        current_id, path = queue.popleft()
+        for neighbor_id in adjacency.get(current_id, []):
+            if neighbor_id in visited:
+                continue
+            visited.add(neighbor_id)
+            next_path = path + [neighbor_id]
+            if neighbor_id == trg_node.id:
+                return [id_to_node[node_id] for node_id in next_path]
+            queue.append((neighbor_id, next_path))
 
-    # getting the neighborhood of a node
-    # checking if the neighborhood contains the trg_node
-    # if not repeat for every node in the neighbor hood and do an expainsion throught the graph
-    # if yes backtrack the path 
+    return None

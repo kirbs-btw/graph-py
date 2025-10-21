6f48752e466951c71331061ccc9886ea569d2e0c
Bastian Lipka <lipka.bastian04@gmail.com>
2025-10-21 23:57:45 +0200

    feat: ontology graph setup

---
+++ src/graph_py/graphs/ontology.py
@@ -0,0 +1,11 @@
+from ..core import Graph, Edge, Node
+
+
+class Ontology(Graph):
+    """
+    defining a graph that describes an ontology
+    an other graph can be checked against it
+    """
+
+    def check_ontology(self):
+        pass
